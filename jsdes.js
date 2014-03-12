672f6bf5f0fbb5d3135787ceafe8fef12a21cd9f
Jimmy Hickey <jameshickey@me.com>
2014-03-12 13:26:12 -0400

    fixesp

--- jsdes.js
+++ jsdes.js
@@ -66,8 +66,7 @@
 
 
 jsdes = function() {
-	this.ascii64 =
-		"./0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+	this.ascii64 = "./0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
 	this.des_IP = [
 		58, 50, 42, 34, 26, 18, 10, 2, 60, 52, 44, 36, 28, 20, 12, 4,
 		62, 54, 46, 38, 30, 22, 14, 6, 64, 56, 48, 40, 32, 24, 16, 8,
