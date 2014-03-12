2502f0e9967bf3e68e8a243b1b5503de544bdbe6
Jimmy Hickey <jameshickey@me.com>
2014-03-12 15:51:57 -0400

    Fixes

--- jsdes.js
+++ jsdes.js
@@ -1,4 +1,4 @@
-module.exports = function() {
+module.exports.jsdes = function() {
 	this.ascii64 = "./0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
 	this.des_IP = [
 		58, 50, 42, 34, 26, 18, 10, 2, 60, 52, 44, 36, 28, 20, 12, 4,
